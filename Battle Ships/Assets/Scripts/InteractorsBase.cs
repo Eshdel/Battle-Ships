1609d48f9fbe5ccba277988541e13193908b9847
EshDel <gagin_ilya@list.ru>
2022-08-10 18:58:04 +0700

    test

---
+++ Battle Ships/Assets/Scripts/InteractorsBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleShips.Architecture {
+    public class InteractorsBase {
+
+        //private Dictionary<Types,Interactor> interactorsMap;
+    
+    }
+}
